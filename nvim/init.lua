fcec1376d23057dde96da18a60c6f748f21d1430
raf <rraf@tuta.io>
2022-08-27 10:31:18 +0100

    update: added platform specific nvim configuration, started with platform specific clipboards

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,23 @@
 require('base')
 require('highlights')
 require('maps')
+
+local has = function(x)
+  return vim.fn.has(x) == 1
+end
+
+local is_unix = has "unix"
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_unix then
+  require('unix')
+end
+
+if is_mac then 
+  require('macos')
+end
+
+if is_win then
+  require('windows')
+end
---
+++ nvim/lua/macos.lua
@@ -0,0 +1,1 @@
+vim.opt.cliboard:append { 'unnamedplus' }
