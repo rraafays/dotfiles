adf113e3bfc865a870883dd0491b6ef9ffd4acdf
raf <rraf@tuta.io>
2022-08-27 11:03:18 +0100

    chore: started using Min & added Min to path

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,5 @@ if status is-interactive
 	starship init fish | source
 end
 
-# pnpm
 set -gx PNPM_HOME "/home/raf/.local/share/pnpm"
-set -gx PATH "$PNPM_HOME" $PATH
-# pnpm end
+set -gx PATH /opt/Min $PNPM_HOME $PATH 
