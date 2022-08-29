d177570176c9d92806d74d4095ac39f3bf82819f
raf <rraf@tuta.io>
2022-08-29 08:43:04 +0100

    update: added a bunch of themeing to various apps

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,8 @@ if status is-interactive
 	starship init fish | source
 end
 
+set -gx BAT_THEME "Catppuccin-mocha"
 set -gx PNPM_HOME "/home/raf/.local/share/pnpm"
 set -gx PATH /opt/Min $PNPM_HOME $PATH 
+
+source $HOME/.config/fish/mocha.sh
