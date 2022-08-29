ae3bb5104921921bbcd97e1c012fc4b13039ef42
raf <rraf@tuta.io>
2022-08-29 11:23:52 +0100

    update: changed zathura to gruvbox dark theme

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
 	starship init fish | source
 end
 
-set -gx BAT_THEME "Catppuccin-mocha"
+set -gx BAT_THEME "gruvbox-dark"
 set -gx PNPM_HOME "/home/raf/.local/share/pnpm"
 set -gx PATH /opt/Min $PNPM_HOME $PATH 
 
