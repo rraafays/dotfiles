83aee18c3802293f0bccff20f4023618be98784c
rraafays <rraf@tuta.io>
2022-08-30 21:50:16 +0100

    chore: got indent-blankline to use treesitter to determine pairs

--- nvim/after/plugin/indent-blankline.rc.lua
+++ nvim/after/plugin/indent-blankline.rc.lua
@@ -4,5 +4,7 @@ if (not status) then return end
 vim.cmd [[highlight IndentBlanklineIndent1 guifg=#665c54 gui=nocombine]]
 
 blankline.setup {
-  char_highlight_list = { "IndentBlanklineIndent1" }
+  char_highlight_list = { "IndentBlanklineIndent1" },
+  indent_blankline_use_treesitter = true,
+  indent_blankline_use_treesitter_scope = true
 }
