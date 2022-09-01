378fb90aa019bfc9357fb35fa3d31228d995ff72
raf <rraf@tuta.io>
2022-09-01 09:26:27 +0100

    update: started using fish in vi mode

--- fish/config.fish
+++ fish/config.fish
@@ -9,3 +9,9 @@ set -gx YARN_INSTALL "$HOME/.yarn"
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 
 set -gx PATH $BUN_INSTALL/bin $YARN_INSTALL/bin $PNPM_HOME $PATH 
+
+
+set fish_cursor_default     block       blink
+set fish_cursor_insert       line        blink
+set fish_cursor_replace_one underscore  blink
+set fish_cursor_visual      block
