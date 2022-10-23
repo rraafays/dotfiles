e7101515ff51fb2bdf35ae24e1c474dedd51db81
raf <rraf@tuta.io>
2022-10-23 16:15:35 +0100

    update: removed italics and added inverted colour on visual

--- nvim/after/plugin/gruvbox.rc.lua
+++ nvim/after/plugin/gruvbox.rc.lua
@@ -3,5 +3,8 @@ if (not status) then return end
 
 n.setup({ comment_italics = true })
 
-require('gruvbox').setup()
+require('gruvbox').setup({
+  italic = false,
+  invert_selection = true,
+})
 vim.cmd [[colorscheme gruvbox]]
