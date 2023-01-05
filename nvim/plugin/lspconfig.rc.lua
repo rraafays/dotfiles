df349bd609d9b7dc56df3004151123659151628b
raf <rraf@tuta.io>
2023-01-05 13:01:46 +0000

    update: reverted to clangd

--- nvim/plugin/lspconfig.rc.lua
+++ nvim/plugin/lspconfig.rc.lua
@@ -54,6 +54,7 @@ nvim_lsp.sumneko_lua.setup {
 nvim_lsp.omnisharp.setup {
   cmd = { "dotnet", "/home/raf/.local/share/nvim/mason/packages/omnisharp/OmniSharp.dll" }
 }
+nvim_lsp.clangd.setup {}
 nvim_lsp.bashls.setup {}
 nvim_lsp.jdtls.setup {}
 nvim_lsp.gradle_ls.setup {}
