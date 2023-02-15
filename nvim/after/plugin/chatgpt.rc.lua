a1a4d45e93ea0b7906659f377ec00d602becae2c
raf <rraf@tuta.io>
2023-02-15 21:35:03 +0000

    chore: refined chatgpt options

--- nvim/after/plugin/chatgpt.rc.lua
+++ nvim/after/plugin/chatgpt.rc.lua
@@ -1,8 +1,7 @@
 local status, chatgpt = pcall(require, "chatgpt")
 if (not status) then return end
 
-vim.keymap.set('n', '<A-g>', ':ChatGPT<CR>')
-vim.keymap.set('n', '<A-a>', ':ChatGPTActAs<CR>')
-vim.keymap.set('n', '<A-c>', ':ChatGPTCompleteCode<CR>')
+vim.keymap.set('n', '<A-c>', ':ChatGPT<CR>')
+vim.keymap.set('n', '<A-i>', ':ChatGPTEditWithInstructions<CR>')
 
 chatgpt.setup()
