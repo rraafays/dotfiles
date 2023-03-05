3d27ef83d051ef7687ff60c9412233ded90e7734
raf <rraf@tuta.io>
2023-03-05 18:44:01 +0000

    fix: removed outdated lua ls on attach

--- nvim/plugin/lspconfig.rc.lua
+++ nvim/plugin/lspconfig.rc.lua
@@ -3,16 +3,6 @@ if (not status) then return end
 
 local protocol = require('vim.lsp.protocol')
 
-local on_attach = function(client, bufnr)
-  -- formatting
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_command [[augroup Format]]
-    vim.api.nvim_command [[autocmd! * <buffer>]]
-    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-    vim.api.nvim_command [[augroup END]]
-  end
-end
-
 vim.fn.sign_define(
   "DiagnosticSignError",
   { texthl = "DiagnosticSignError", text = "", numhl = "DiagnosticSignError" }
@@ -31,12 +21,10 @@ vim.fn.sign_define(
 )
 
 nvim_lsp.tsserver.setup {
-  on_attach = on_attach,
   filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" },
   cmd = { "typescript-language-server", "--stdio" }
 }
 nvim_lsp.lua_ls.setup {
-  on_attach = on_attach,
   settings = {
     Lua = {
       diagnostics = {
