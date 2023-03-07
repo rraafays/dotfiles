5b43bab61a4b98be46935f91ba0a05b199139b9a
raf <rraf@tuta.io>
2023-03-07 13:43:30 +0000

    update: gruvbox colours

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Color Palette
-export BLACK=0xffFF0000
+export BLACK=0xff282828
 export WHITE=0xffEBDBB2
-export RED=0xffed8796
-export GREEN=0xffa6da95
-export BLUE=0xff8aadf4
-export YELLOW=0xffeed49f
-export ORANGE=0xfff5a97f
-export MAGENTA=0xffc6a0f6
-export GREY=0xff939ab7
+export RED=0xffCC241D
+export GREEN=0xff98971A
+export BLUE=0xff258588
+export YELLOW=0xffD79921
+export ORANGE=0xffD65D0E
+export MAGENTA=0xffB16286
+export GREY=0xff928374
 export TRANSPARENT=0x00000000
 
 # General bar colors
 export BAR_COLOR=0xff282828
-export BAR_BORDER_COLOR=0xff494d64 #0xa024273a
+export BAR_BORDER_COLOR=0xffFF0000 #0xa024273a
 export ICON_COLOR=$WHITE # Color of all icons
 export LABEL_COLOR=$WHITE # Color of all labels
-export BACKGROUND_1=0x603c3e4f
-export BACKGROUND_2=0x60494d64
+export BACKGROUND_1=0xFF3C3836
+export BACKGROUND_2=0xFF504945
 
 export POPUP_BACKGROUND_COLOR=0xff1e1e2e
 export POPUP_BORDER_COLOR=$WHITE
