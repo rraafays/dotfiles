d1a9c30ca1647020977366150da174b0c6e9a49e
raf <rraf@tuta.io>
2023-03-07 13:02:01 +0000

    chore: reorganised plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -20,33 +20,33 @@ end
 
 -- starts lazy
 lazy.setup({
-    defaults = { lazy = true },
-     'l3mon4d3/luasnip', --snippets for completions
-     'hoob3rt/lualine.nvim', -- replacement statusline
-     'onsails/lspkind-nvim', -- vscode style pictograms
-     'hrsh7th/cmp-buffer', -- nvim-cmp source for buffer words
-     'hrsh7th/cmp-nvim-lsp', -- nvim-cmp source for neovim's built in lsp
-     'hrsh7th/nvim-cmp', -- language completions
-     'neovim/nvim-lspconfig', -- language server protocol
-     'williamboman/mason.nvim', -- language server installer
-     'windwp/nvim-autopairs', -- auto closing brackets and quotations
-     'windwp/nvim-ts-autotag', -- auto closing typescript tags
-     'ellisonleao/gruvbox.nvim', -- gruvbox theme
-     'nvim-treesitter/nvim-treesitter', -- accurate code tree highlighting
-     'nvim-lua/plenary.nvim', -- common file utilities
-     'nvim-telescope/telescope.nvim', -- fuzzy finder
-     'nvim-telescope/telescope-file-browser.nvim', -- file explorer
-     'kyazdani42/nvim-web-devicons', -- file icons
-     'norcalli/nvim-colorizer.lua', -- colour highlights hex, rgb and more
-     'glepnir/lspsaga.nvim', -- lsp uis
-     'lewis6991/gitsigns.nvim', -- git markers
-     'dinhhuy258/git.nvim', -- git blame & browse
-     'kevinhwang91/promise-async', -- promise and async
-     'lervag/vimtex', -- latex auto compiler
-     'folke/zen-mode.nvim', -- zen mode
-     'elkowar/yuck.vim', -- yuck lsp
-     'muniftanjim/nui.nvim', -- ui library
-     'jackmort/chatgpt.nvim', -- chatgpt querying
-     'kylechui/nvim-surround', -- surround items
-     'johmsalas/text-case.nvim' -- snake case everything
+  defaults = { lazy = true },
+  'l3mon4d3/luasnip', --snippets for completions
+  'hoob3rt/lualine.nvim', -- replacement statusline
+  'onsails/lspkind-nvim', -- vscode style pictograms
+  'hrsh7th/cmp-buffer', -- nvim-cmp source for buffer words
+  'hrsh7th/cmp-nvim-lsp', -- nvim-cmp source for neovim's built in lsp
+  'hrsh7th/nvim-cmp', -- language completions
+  'neovim/nvim-lspconfig', -- language server protocol
+  'williamboman/mason.nvim', -- language server installer
+  'windwp/nvim-autopairs', -- auto closing brackets and quotations
+  'windwp/nvim-ts-autotag', -- auto closing typescript tags
+  'ellisonleao/gruvbox.nvim', -- gruvbox theme
+  'nvim-treesitter/nvim-treesitter', -- accurate code tree highlighting
+  'nvim-lua/plenary.nvim', -- common file utilities
+  'nvim-telescope/telescope.nvim', -- fuzzy finder
+  'nvim-telescope/telescope-file-browser.nvim', -- file explorer
+  'kyazdani42/nvim-web-devicons', -- file icons
+  'norcalli/nvim-colorizer.lua', -- colour highlights hex, rgb and more
+  'glepnir/lspsaga.nvim', -- lsp uis
+  'lewis6991/gitsigns.nvim', -- git markers
+  'dinhhuy258/git.nvim', -- git blame & browse
+  'kevinhwang91/promise-async', -- promise and async
+  'lervag/vimtex', -- latex auto compiler
+  'folke/zen-mode.nvim', -- zen mode
+  'elkowar/yuck.vim', -- yuck lsp
+  'muniftanjim/nui.nvim', -- ui library
+  'jackmort/chatgpt.nvim', -- chatgpt querying
+  'kylechui/nvim-surround', -- surround items
+  'johmsalas/text-case.nvim' -- snake case everything
 })
