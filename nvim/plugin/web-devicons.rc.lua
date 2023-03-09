14b06e5b4956a24430f1fe3ecd726141fc7441af
raf <rraf@tuta.io>
2023-03-09 21:02:31 +0000

    update: rust

--- nvim/plugin/web-devicons.rc.lua
+++ nvim/plugin/web-devicons.rc.lua
@@ -2,6 +2,12 @@ local status, icons = pcall(require, 'nvim-web-devicons')
 if (not status) then return end
 
 icons.setup {
-  override = {},
-  default = true
+  override = {
+    rs = {
+      icon = '',
+      color = '#F74C00',
+      cterm_color = '#F74C00',
+      name = 'rust'
+    }
+  }
 }
