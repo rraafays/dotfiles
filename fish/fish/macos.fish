3bbd8703ab8c04d5f819587cd43e77f42131f03e
raf <rraf@tuta.io>
2023-03-13 21:48:23 +0000

    chore: renamed from update to upgrade

--- fish/fish/macos.fish
+++ fish/fish/macos.fish
@@ -1,4 +1,4 @@
 function meh -d 'feh for mac by drabweb'; open -b "drabweb.macfeh" $argv; end # feh-like alternative for mac
 set -x ANDROID_HOME "$HOME/Library/Android/sdk"
 function man -d 'man'; command man -P 'nvim -c \'Man! -o -\'' $argv; end
-function brew-update -d 'greedy brew'; brew upgrade --greedy; end
+function brew-upgrade -d 'greedy brew'; brew upgrade --greedy; end
