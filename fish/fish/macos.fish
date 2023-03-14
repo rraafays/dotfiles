590a97e007fb448e0e1ca7f937956ba95728e860
raf <rraf@tuta.io>
2023-03-14 16:38:11 +0000

    update: make omnisharp use mono for unity support

--- fish/fish/macos.fish
+++ fish/fish/macos.fish
@@ -1,4 +1,5 @@
 function meh -d 'feh for mac by drabweb'; open -b "drabweb.macfeh" $argv; end # feh-like alternative for mac
 set -x ANDROID_HOME "$HOME/Library/Android/sdk"
+set -x FrameworkPathOverride "$HOME/Library/Frameworks/Mono.framework/Versions/Current/Commands/mono"
 function man -d 'man'; command man -P 'nvim -c \'Man! -o -\'' $argv; end
 function brew-upgrade -d 'greedy brew'; brew upgrade --greedy; end
