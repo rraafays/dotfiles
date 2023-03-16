9378899425a399b0753cbe878808e3e8406b05a1
raf <rraf@tuta.io>
2023-03-16 21:36:42 +0000

    update: imported default ls colours

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,7 @@ source $FISH_HOME/fish/aliases.fish
 source $FISH_HOME/fish/keys.fish
 source $FISH_HOME/fish/functions.fish
 source $FISH_HOME/fish/path.fish
+source $FISH_HOME/fish/ls.fish
 
 # source platform specific scripts
 switch (uname)
