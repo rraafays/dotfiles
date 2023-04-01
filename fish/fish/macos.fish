5bf05953e060b686104f7a76ba85c40ce01b0a82
raf <rraf@tuta.io>
2023-04-02 00:47:28 +0100

    chore: removed greedy brew

--- fish/fish/macos.fish
+++ fish/fish/macos.fish
@@ -2,4 +2,3 @@ function meh -d 'feh for mac by drabweb'; open -b "drabweb.macfeh" $argv; end # 
 set -x ANDROID_HOME "$HOME/Library/Android/sdk"
 set -x FrameworkPathOverride "$HOME/Library/Frameworks/Mono.framework/Versions/Current/Commands/mono"
 function man -d 'man'; command man -P 'nvim -c \'Man! -o -\'' $argv; end
-function brew-upgrade -d 'greedy brew'; brew upgrade --greedy; end
