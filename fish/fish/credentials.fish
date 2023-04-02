3b6f6ac8e698fed61f186acbbec2e3761edfa4a4
raf <rraf@tuta.io>
2023-04-02 12:24:47 +0100

    update: streamlined credentials

--- fish/fish/credentials.fish
+++ fish/fish/credentials.fish
@@ -1,3 +1,6 @@
 set CREDENTIALS "$FISH_HOME/credentials"
+if not test -e $CREDENTIALS; mkdir $CREDENTIALS; end
+
+if not test -e $CREDENTIALS/soulseek.fish; touch $CREDENTIALS/soulseek.fish; end
 
 source $CREDENTIALS/soulseek.fish
