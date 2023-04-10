46374a741333f5d3ab051bfc707b70fb6972a681
raf <rraf@tuta.io>
2023-04-10 09:06:52 +0100

    chore: added some nice spacing to the noice prompts

--- nvim/plugin/noice.rc.lua
+++ nvim/plugin/noice.rc.lua
@@ -13,10 +13,10 @@ noice.setup {
       -- opts: any options passed to the view
       -- icon_hl_group: optional hl_group for the icon
       -- title: set to anything or empty string to hide
-      cmdline = { pattern = "^:", icon = ":", lang = "vim" },
-      search_down = { kind = "search", pattern = "^/", icon = "/", lang = "regex" },
-      search_up = { kind = "search", pattern = "^%?", icon = "/", lang = "regex" },
-      filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+      cmdline = { pattern = "^:", icon = " :", lang = "vim" },
+      search_down = { kind = "search", pattern = "^/", icon = " /", lang = "regex" },
+      search_up = { kind = "search", pattern = "^%?", icon = " /", lang = "regex" },
+      filter = { pattern = "^:%s*!", icon = " $", lang = "bash" },
       lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
       help = { pattern = "^:%s*he?l?p?%s+", icon = "?" },
       input = {}, -- Used by input()
