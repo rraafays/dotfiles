baa95a7fc26101df7de274227382e7a78153f065
raf <rraf@tuta.io>
2023-04-24 22:22:10 +0100

    chore: added some mac specific tools

--- fish/fish/macos.fish
+++ fish/fish/macos.fish
@@ -1,3 +1,5 @@
 set -x ANDROID_HOME "$HOME/Library/Android/sdk"
 set -x FrameworkPathOverride "$HOME/Library/Frameworks/Mono.framework/Versions/Current/Commands/mono"
 function man -d 'man'; command man -P 'nvim -c \'Man! -o -\'' $argv; end
+function fixlogin -d 'fix login items and background tasks'; sfltool resetbtm; end
+function doas -d 'alias doas to sudo'; sudo $argv; end
