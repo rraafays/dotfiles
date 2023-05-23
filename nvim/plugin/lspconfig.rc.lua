3122dcca66525c5faa412d23f5efeffcbdd5c22e
raf <rraf@tuta.io>
2023-05-23 12:30:51 +0100

    chore: slight restructuring, prepping for super fancy rust config

--- nvim/plugin/lspconfig.rc.lua
+++ nvim/plugin/lspconfig.rc.lua
@@ -1,39 +1,13 @@
 local status, nvim_lsp = pcall(require, 'lspconfig')
 if (not status) then return end
 
-local protocol = require('vim.lsp.protocol')
-
-vim.fn.sign_define(
-  "DiagnosticSignError",
-  { texthl = "DiagnosticSignError", text = "", numhl = "DiagnosticSignError" }
-)
-vim.fn.sign_define(
-  "DiagnosticSignWarn",
-  { texthl = "DiagnosticSignWarn", text = "", numhl = "DiagnosticSignWarn" }
-)
-vim.fn.sign_define(
-  "DiagnosticSignHint",
-  { texthl = "DiagnosticSignHint", text = "", numhl = "DiagnosticSignHint" }
-)
-vim.fn.sign_define(
-  "DiagnosticSignInfo",
-  { texthl = "DiagnosticSignInfo", text = "", numhl = "DiagnosticSignInfo" }
-)
-
-vim.g.rust_recommended_style = false
+vim.g.rust_recommended_style = true
 nvim_lsp.rust_analyzer.setup {
   settings = {
-    [ "rust-analyzer" ] = { diagnostics = { disabled = { "unresolved-proc-macro" } } }
+    [ "rust-analyzer" ] = {
+      diagnostics = { disabled = { "unresolved-proc-macro" } }
+    }
   }
-}
-nvim_lsp.csharp_ls.setup {}
-nvim_lsp.clangd.setup {}
-nvim_lsp.bashls.setup {}
-nvim_lsp.pylsp.setup {}
-nvim_lsp.ltex.setup {}
-nvim_lsp.tsserver.setup {
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" },
-  cmd = { "typescript-language-server", "--stdio" }
 }
 nvim_lsp.lua_ls.setup {
   settings = {
@@ -54,3 +28,29 @@ nvim_lsp.lua_ls.setup {
     },
   },
 }
+nvim_lsp.csharp_ls.setup {}
+nvim_lsp.clangd.setup {}
+nvim_lsp.bashls.setup {}
+nvim_lsp.pylsp.setup {}
+nvim_lsp.ltex.setup {}
+nvim_lsp.tsserver.setup {
+  filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" },
+  cmd = { "typescript-language-server", "--stdio" }
+}
+
+vim.fn.sign_define(
+  "DiagnosticSignError",
+  { texthl = "DiagnosticSignError", text = "", numhl = "DiagnosticSignError" }
+)
+vim.fn.sign_define(
+  "DiagnosticSignWarn",
+  { texthl = "DiagnosticSignWarn", text = "", numhl = "DiagnosticSignWarn" }
+)
+vim.fn.sign_define(
+  "DiagnosticSignHint",
+  { texthl = "DiagnosticSignHint", text = "", numhl = "DiagnosticSignHint" }
+)
+vim.fn.sign_define(
+  "DiagnosticSignInfo",
+  { texthl = "DiagnosticSignInfo", text = "", numhl = "DiagnosticSignInfo" }
+)
