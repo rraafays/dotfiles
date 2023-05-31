a32be3b301e4eec4737e33cd17dd3c8fea2ecaaa
raf <rraf@tuta.io>
2023-05-31 22:40:02 +0100

    update: added more black background

--- fish/fish/unix.fish
+++ fish/fish/unix.fish
@@ -10,7 +10,7 @@ set -x LIBVA_DRIVER_NAME nvidia
 set -x XDG_SESSION_TYPE wayland
 set -x GBM_BACKEND nvidia
 
-set -x BEMENU_OPTS "--fn 'Iosevka Term 12' --prompt > --tb #282828FF --tf #D65C0DFF --fb #282828FF --ff #EBDBB2FF --cb #EBDBB2FF --cf #EBDBB2FF --nb #282828FF --nf #928373FF --ab #282828FF --af #928373FF --hb #282828FF --hf #EBDBB2FF"
+set -x BEMENU_OPTS "--fn 'Iosevka Term 12' --prompt > --tb #000000FF --tf #D65C0DFF --fb #000000FF --ff #EBDBB2FF --cb #EBDBB2FF --cf #EBDBB2FF --nb #000000FF --nf #928373FF --ab #000000FF --af #928373FF --hb #000000FF --hf #EBDBB2FF"
 set -x ANDROID_HOME "$HOME/Android/Sdk"
 
 function hypr -d 'launch hyprland'; Hyprland; end
