7543d77a4dd8562cda8482aba8d5676a7f93d864
raf <rraf@tuta.io>
2023-06-09 23:15:21 +0100

    chore: properly added brew libs

--- fish/fish/macos.fish
+++ fish/fish/macos.fish
@@ -1,3 +1,4 @@
+set -x LIBRARY_PATH "$LIBRARY_PATH:$(brew --prefix)/lib"
 set -x ANDROID_HOME "$HOME/Library/Android/sdk"
 set -x FrameworkPathOverride "$HOME/Library/Frameworks/Mono.framework/Versions/Current/Commands/mono"
 function man -d 'man'; command man -P 'nvim -c \'Man! -o -\'' $argv; end
