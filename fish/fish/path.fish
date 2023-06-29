6b488c54f63d9c136f0943eff4f6bcd6c036fe8d
raf <rraf@tuta.io>
2023-06-29 23:14:00 +0100

    chore: added mac users bins

--- fish/fish/path.fish
+++ fish/fish/path.fish
@@ -6,5 +6,6 @@ if [ "$GO_INSTALL" != "$HOME/go" ]; set -x GO_INSTALL "$HOME/go"; end
 if [ "$RBENV_INSTALL" != "$HOME/.rbenv" ]; set -x RBENV_INSTALL "$HOME/.rbenv"; end
 if [ "$BREW_INSTALL" != "/opt/homebrew" ]; set -x BREW_INSTALL "/opt/homebrew"; end
 if [ "$PNPM_HOME" != "$HOME/.local/share/pnpm" ]; set -x PNPM_HOME "$HOME/.local/share/pnpm"; end
+if [ "$USER_HOME" != "/Users/$USER" ]; set -x USER_HOME "/Users/$USER"; end
 
-set -x PATH $BUN_INSTALL/bin $YARN_INSTALL/bin  $PNPM_HOME $CARGO_INSTALL/bin $DOTNET_INSTALL/tools $BREW_INSTALL/sbin $BREW_INSTALL/bin $GO_INSTALL/bin $RBENV_INSTALL/bin $RBENV_INSTALL/shims $HOME/.local/bin $HOME/.config/fish/scripts $PATH 
+set -x PATH $BUN_INSTALL/bin $YARN_INSTALL/bin $PNPM_HOME $CARGO_INSTALL/bin $DOTNET_INSTALL/tools $BREW_INSTALL/sbin $BREW_INSTALL/bin $GO_INSTALL/bin $RBENV_INSTALL/bin $RBENV_INSTALL/shims $HOME/.local/bin $HOME/.config/fish/scripts $USER_HOME/bin $PATH 
