9f85581c71182e6dca3314055b801cb6fc17a7aa
raf <rraf@tuta.io>
2023-06-30 06:58:27 +0100

    fix: got tty independent theme working

--- nvim/plugin/gruvbox.rc.lua
+++ nvim/plugin/gruvbox.rc.lua
@@ -10,7 +10,7 @@ gruvbox.setup{
     }
 }
 
-if os.getenv("DISPLAY") == "" then
+if os.getenv("DISPLAY") == nil then
     vim.cmd [[ colorscheme default ]]
 else
     vim.cmd [[ colorscheme gruvbox ]]
