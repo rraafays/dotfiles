981bdad6f49776df5ccad6fde29258cc6ca1d3c3
raf <rraf@tuta.io>
2023-07-19 06:04:16 +0100

    chore: run all skhd and yabai commands in zsh for speed

--- fish/fish/macos.fish
+++ fish/fish/macos.fish
@@ -2,9 +2,21 @@ set -x LIBRARY_PATH "$LIBRARY_PATH:$(brew --prefix)/lib"
 set -x ANDROID_HOME "$HOME/Library/Android/sdk"
 set -x FrameworkPathOverride "$HOME/Library/Frameworks/Mono.framework/Versions/Current/Commands/mono"
 
-function man -d 'man'; command man -P 'nvim -c \'Man! -o -\'' $argv; end
-function fixlogin -d 'fix login items and background tasks'; sfltool resetbtm; end
-function doas -d 'alias doas to sudo'; sudo $argv; end
+function man -d 'man'
+    command man -P 'nvim -c \'Man! -o -\'' $argv
+end
+function fixlogin -d 'fix login items and background tasks'
+    sfltool resetbtm
+end
+function doas -d 'alias doas to sudo'
+    sudo $argv
+end
+function yabai -d 'run yabai in zsh'
+    env SHELL=/bin/zsh yabai $argv
+end
+function skhd -d 'run skhd in zsh'
+    env SHELL=/bin/zsh skhd $argv
+end
 
 if not test -e "$HOME/.config/kitty/kitty.conf"
     cat                                     \
--- fish/fish/unix.fish
+++ fish/fish/unix.fish
@@ -3,7 +3,9 @@ if [ "$MANPAGER" != "nvim -c 'Man! -o -'" ]; set MANPAGER "nvim -c 'Man! -o -'";
 set -x BEMENU_OPTS "--fn 'Iosevka Term 12' --prompt > --tb #000000FF --tf #D65C0DFF --fb #000000FF --ff #EBDBB2FF --cb #EBDBB2FF --cf #EBDBB2FF --nb #000000FF --nf #928373FF --ab #000000FF --af #928373FF --hb #000000FF --hf #EBDBB2FF"
 set -x ANDROID_HOME "$HOME/Android/Sdk"
 
-function hypr -d 'launch hyprland'; Hyprland; end
+function hypr -d 'launch hyprland'
+    Hyprland
+end
 
 if not test -e "$HOME/.config/kitty/kitty.conf"
     cat                                     \
