c3601c9bba75b54a5813a8b160208b2a02ff4c79
raf <rraf@tuta.io>
2023-07-29 19:18:16 +0100

    chore: made bemenu match gruvbox colours

--- fish/fish/unix.fish
+++ fish/fish/unix.fish
@@ -2,7 +2,7 @@ if [ "$MANPAGER" != "nvim -c 'Man! -o -'" ]
   set MANPAGER "nvim -c 'Man! -o -'"
 end
 
-set -x BEMENU_OPTS "--fn 'Iosevka Term 12' --prompt > --tb #000000FF --tf #EA2300FF --fb #000000FF --ff #FAFFB2FF --cb #FAFFB2FF --cf #FAFFB2FF --nb #000000FF --nf #F2F2C4FF --ab #000000FF --af #F2F2C4FF --hb #000000FF --hf #FAFFB2FF"
+set -x BEMENU_OPTS "--fn 'Iosevka Term 12' --prompt > --tb #000000FF --tf #FE8019FF --fb #000000FF --ff #EBDBB2FF --cb #000000FF --cf #EBDBB2FF --nb #000000FF --nf #A89983FF --ab #000000FF --af #A89983FF --hb #000000FF --hf #8EC07BFF"
 set -x ANDROID_HOME "$HOME/Android/Sdk"
 
 function hypr -d 'launch hyprland'
