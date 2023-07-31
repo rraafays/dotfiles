74a63e8e992494483e187d7db8c787d59a2de44a
raf <rraf@tuta.io>
2023-07-31 11:43:44 +0100

    update: improved credentials script

--- fish/fish/credentials.fish
+++ fish/fish/credentials.fish
@@ -1,6 +1,9 @@
 set CREDENTIALS "$FISH_HOME/credentials"
 if not test -e $CREDENTIALS; mkdir $CREDENTIALS; end
 
-if not test -e $CREDENTIALS/soulseek.fish; touch $CREDENTIALS/soulseek.fish; end
+if not test -e $CREDENTIALS/soulseek.fish
+  echo "set -x SOULSEEK_ACCOUNT \"\"" >> $CREDENTIALS/soulseek.fish
+  echo "set -x SOULSEEK_PASSWORD \"\"" >> $CREDENTIALS/soulseek.fish
+end
 
 source $CREDENTIALS/soulseek.fish
