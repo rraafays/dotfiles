1c909b878406faeb4a3ee0c713ba92c9afb8f1eb
raf <rraf@tuta.io>
2023-08-01 23:27:16 +0100

    update: added support for pop via resend api key

--- fish/fish/credentials.fish
+++ fish/fish/credentials.fish
@@ -5,5 +5,9 @@ if not test -e $CREDENTIALS/soulseek.fish
   echo "set -x SOULSEEK_ACCOUNT \"\"" >> $CREDENTIALS/soulseek.fish
   echo "set -x SOULSEEK_PASSWORD \"\"" >> $CREDENTIALS/soulseek.fish
 end
+if not test -e $CREDENTIALS/resend.fish
+  echo "set -x RESEND_API_KEY \"\"" >> $CREDENTIALS/resend.fish
+end
 
 source $CREDENTIALS/soulseek.fish
+source $CREDENTIALS/resend.fish
