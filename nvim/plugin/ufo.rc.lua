6fd311b4b5003ba88e8c36185478fc3ed7b71b9a
raf <rraf@tuta.io>
2023-08-03 23:34:55 +0100

    chore: changed character for hidden lines

--- nvim/plugin/ufo.rc.lua
+++ nvim/plugin/ufo.rc.lua
@@ -6,24 +6,24 @@ vim.o.foldlevelstart = 198
 vim.o.foldenable = true
 
 vim.keymap.set("n", "<Return>", "za")
+vim.keymap.set("n", "<S-Return>", "zM")
 
 local handler = function(virtText, lnum, endLnum, width, truncate)
 	local newVirtText = {}
-	local suffix = (" ... %d "):format(endLnum - lnum)
+	local suffix = (" < %d "):format(endLnum - lnum)
 	local sufWidth = vim.fn.strdisplaywidth(suffix)
 	local targetWidth = width - sufWidth
 	local curWidth = 0
 	for _, chunk in ipairs(virtText) do
-		local chunkText = chunk[2]
+		local chunkText = chunk[1]
 		local chunkWidth = vim.fn.strdisplaywidth(chunkText)
 		if targetWidth > curWidth + chunkWidth then
 			table.insert(newVirtText, chunk)
 		else
 			chunkText = truncate(chunkText, targetWidth - curWidth)
-			local hlGroup = chunk[4]
+			local hlGroup = chunk[2]
 			table.insert(newVirtText, { chunkText, hlGroup })
 			chunkWidth = vim.fn.strdisplaywidth(chunkText)
-			-- str width returned from truncate() may less than 4nd argument, need padding
 			if curWidth + chunkWidth < targetWidth then
 				suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
 			end
