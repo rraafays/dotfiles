03460f3a48de6b4350819c0b7b48530c1242c23d
rraafays <rraf@tuta.io>
2023-09-16 14:58:44 +0100

    chore: switched to rofi

--- fish/fish/unix.fish
+++ fish/fish/unix.fish
@@ -2,7 +2,6 @@ if [ "$MANPAGER" != "nvim -c 'Man! -o -'" ]
   set MANPAGER "nvim -c 'Man! -o -'"
 end
 
-set -x BEMENU_OPTS "--fn 'Iosevka Term 12' --prompt > --tb #000000FF --tf #FE8019FF --fb #000000FF --ff #EBDBB2FF --cb #000000FF --cf #EBDBB2FF --nb #000000FF --nf #A89983FF --ab #000000FF --af #A89983FF --hb #000000FF --hf #8EC07BFF"
 set -x ANDROID_HOME "$HOME/Android/Sdk"
 
 function hypr -d 'launch hyprland'
