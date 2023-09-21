3ad0a979ae274293de21fc023774ae9f2a72906d
raf <rraf@tuta.io>
2023-09-21 13:45:00 +0100

    chore: made gh dir same as home so that i can cache both root and user auth

--- .base/fish/fish/base.fish
+++ .base/fish/fish/base.fish
@@ -1,4 +1,5 @@
 set -x EDITOR nvim
+set -x GH_CONFIG_DIR "$HOME"
 set -x FZF_DEFAULT_COMMAND "rg --files --hidden --follow"
 set -x FZF_DEFAULT_OPTS "--color="16,bg+:#000000""
 set -x GRAVEYARD "$HOME/.graveyard"
