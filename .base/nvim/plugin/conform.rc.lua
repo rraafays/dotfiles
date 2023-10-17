56e44ea05e024fbff2ad8a506a0b544930c92fc6
raf <rraf@tuta.io>
2023-10-17 12:12:26 +0100

    fix: some formatters arent setup properly

--- .base/nvim/plugin/conform.rc.lua
+++ .base/nvim/plugin/conform.rc.lua
@@ -1,23 +1,29 @@
 local conform = require("conform")
 
 conform.setup({
+	format_on_save = {
+		lsp_fallback = true,
+		timeout_ms = 500,
+	},
+	format_after_save = {
+		lsp_fallback = true,
+	},
 	formatters_by_ft = {
-		["*"] = { "trim_whitespace" },
 		lua = { "stylua" },
-		sql = { "sqlfmt" },
 		xml = { "tidy" },
-		java = {
-			command = "clang-format",
-			args = { "--style=Google", "--assume-filename=.java" },
-		},
-		javascript = {
-			command = "prettier",
-			args = { "--tab-width 2" },
-		},
-		sh = {
-			command = { "shfmt" },
-			args = { "-i", 2 },
-		},
+		sql = { "sql_formatter" },
+		-- java = {
+		-- 	command = "clang-format",
+		-- 	args = { "--style=Google", "--assume-filename=.java" },
+		-- },
+		-- javascript = {
+		-- 	command = "prettier",
+		-- 	args = { "--tab-width 2" },
+		-- },
+		-- sh = {
+		-- 	command = { "shfmt" },
+		-- 	args = { "-i", 2 },
+		-- },
 	},
 })
 
