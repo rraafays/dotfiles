074ee2ec54fedb587a04e2d0185c0c2db8acc4f2
raf <rraf@tuta.io>
2023-10-18 01:05:58 +0100

    chore: added support for async intellij format just in case

--- .base/nvim/plugin/conform.rc.lua
+++ .base/nvim/plugin/conform.rc.lua
@@ -32,3 +32,26 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 		require("conform").format({ bufnr = args.buf })
 	end,
 })
+
+local slow_format_filetypes = { "java" }
+require("conform").setup({
+	format_on_save = function(bufnr)
+		if slow_format_filetypes[vim.bo[bufnr].filetype] then
+			return
+		end
+		local function on_format(err)
+			if err and err:match("timeout$") then
+				slow_format_filetypes[vim.bo[bufnr].filetype] = true
+			end
+		end
+
+		return { timeout_ms = 200, lsp_fallback = true }, on_format
+	end,
+
+	format_after_save = function(bufnr)
+		if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+			return
+		end
+		return { lsp_fallback = true }
+	end,
+})
