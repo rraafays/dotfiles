7467b5704a8bb0558fb6aa4dbc27cc8a9907b6f0
rraf <rraf@tuta.io>
2023-10-27 18:54:34 +0100

    chore: made some maps silent

--- .base/nvim/lua/maps.lua
+++ .base/nvim/lua/maps.lua
@@ -1,18 +1,18 @@
--- shortens vim.keymap to just keymap by redifining it in lua
 local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 
 -- lazy command
 keymap.set("n", ";", ":")
 
 -- lazy leave
-keymap.set("n", "q", ":q<CR>")
+keymap.set("n", "q", ":q<CR>", opts)
 
 -- easy to reach exit
 keymap.set("v", "q", "<esc>")
 
 -- force quit
-keymap.set("n", "<C-c>", "<Esc>:qa!<CR>")
-keymap.set("i", "<C-c>", "<Esc>:qa!<CR>")
+keymap.set("n", "<C-c>", "<Esc>:qa!<CR>", opts)
+keymap.set("i", "<C-c>", "<Esc>:qa!<CR>", opts)
 
 -- don't yank on delete
 keymap.set("n", "x", '"_x')
@@ -25,7 +25,7 @@ keymap.set("n", "@", ":s/\\d\\+/\\=submatch(0)*2/<CR>n:nohlsearch<CR>")
 keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- unhighlight search
-keymap.set("n", "\\", ":nohlsearch<CR>")
+keymap.set("n", "\\", ":nohlsearch<CR>", opts)
 
 -- quick line del in normal mode
 keymap.set("n", "<BS>", "dd")
@@ -33,20 +33,10 @@ keymap.set("n", "<BS>", "dd")
 -- join to line above
 keymap.set("n", "K", "kJ")
 
--- pane nav
-keymap.set("n", "<A-k>", ":wincmd k<CR>")
-keymap.set("n", "<A-l>", ":wincmd l<CR>")
-keymap.set("n", "<A-j>", ":wincmd j<CR>")
-keymap.set("n", "<A-h>", ":wincmd h<CR>")
-keymap.set("t", "<A-k>", "<cmd> wincmd k<CR>")
-keymap.set("t", "<A-l>", "<cmd> wincmd l<CR>")
-keymap.set("t", "<A-j>", "<cmd> wincmd j<CR>")
-keymap.set("t", "<A-h>", "<cmd> wincmd h<CR>")
-
 -- insert arrows
-keymap.set("i", "<Up>", "￪")
+keymap.set("i", "<Up>", "↑")
 keymap.set("i", "<Right>", "->")
-keymap.set("i", "<Down>", "￬")
+keymap.set("i", "<Down>", "↓")
 keymap.set("i", "<Left>", "<-")
 keymap.set("i", "<S-Up>", "⇑")
 keymap.set("i", "<S-Right>", "=>")
