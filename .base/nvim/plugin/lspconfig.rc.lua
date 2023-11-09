5ebafdeddb83d39c3002fe9c81b89561518b9e41
raf <rraf@tuta.io>
2023-11-09 09:38:50 +0000

    fix: updated lua config

--- .base/nvim/plugin/lspconfig.rc.lua
+++ .base/nvim/plugin/lspconfig.rc.lua
@@ -20,7 +20,7 @@ nvim_lsp.lua_ls.setup({
 			},
 			workspace = {
 				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
+				checkThirdParty = "Disable",
 			},
 			telemetry = {
 				enable = false,
