ef791cfac44bce229969a9684c5aaa7d1eeee8ca
raf <rraf@tuta.io>
2023-12-09 15:11:49 +0000

    chore: added colouriser reload to bufwritepost

--- .base/nvim/plugin/colorizer.rc.lua
+++ .base/nvim/plugin/colorizer.rc.lua
@@ -2,3 +2,8 @@ local status, colorizer = pcall(require, "colorizer")
 if not status then return end
 
 colorizer.setup({ "*" }, { mode = "foreground" })
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "",
+    command = "ColorizerAttachToBuffer",
+})
