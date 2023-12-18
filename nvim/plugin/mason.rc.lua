ff64d55334aa19e976559e6b03145e7f5af70a14
raf <rraf@tuta.io>
2023-12-18 06:43:24 +0000

    chore: update list of lsps to install

--- nvim/plugin/mason.rc.lua
+++ nvim/plugin/mason.rc.lua
@@ -3,20 +3,22 @@ local mason_lsp = require("mason-lspconfig")
 
 mason.setup({})
 mason_lsp.setup({
-	ensure_installed = {
-		"astro",
-		"bashls",
-		"clangd",
-		"csharp_ls",
-		"cssls",
-		"html",
-		"jdtls",
-		"ltex",
-		"lua_ls",
-		"pylsp",
-		"rust_analyzer",
-		"sqlls",
-		"taplo",
-		"tsserver",
-	},
+    ensure_installed = {
+        "jdtls",
+        "astro",
+        "bashls",
+        "clangd",
+        "csharp_ls",
+        "cssls",
+        "elmls",
+        "html",
+        "lemminx",
+        "ltex",
+        "lua_ls",
+        "pylsp",
+        "rust_analyzer",
+        "sqlls",
+        "taplo",
+        "tsserver",
+    },
 })
