593aaf0f5b3ea453fa5915f1141e18d113501654
rraf@tuta.io <rraf@tuta.io>
2023-12-21 19:39:28 +0000

    fix: made sdl programs use wayland

--- fish/fish/base.fish
+++ fish/fish/base.fish
@@ -1,8 +1,8 @@
 set -x EDITOR nvim
+set -x SDL_VIDEODRIVER wayland
 set -x GH_CONFIG_DIR "$HOME/.config/gh"
 set -x FZF_DEFAULT_COMMAND "rg --files --follow --no-ignore --no-ignore-dot --no-ignore-exclude --no-ignore-files --no-ignore-global --no-ignore-parent --no-ignore-vcs"
 set -x FZF_DEFAULT_OPTS "--color="16,bg+:#000000""
-set -x ELINKS_CONFDIR "$HOME/.config/elinks"
 
 if status is-interactive
     starship init fish | source
