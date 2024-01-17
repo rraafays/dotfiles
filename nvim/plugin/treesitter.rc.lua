32893ed2af446144b7f54a0445327d204a62a81d
raf <rraf@tuta.io>
2024-01-17 06:20:41 +0000

    chore: added vimdoc

--- nvim/plugin/treesitter.rc.lua
+++ nvim/plugin/treesitter.rc.lua
@@ -2,29 +2,30 @@ local configs = require("nvim-treesitter.configs")
 local treesitter = require("nvim-treesitter")
 
 configs.setup({
-	build = ":TSUpdate",
-	highlight = {
-		enable = true,
-		disable = {},
-	},
-	indent = {
-		enable = true,
-		disable = {},
-	},
-	ensure_installed = {
-		"lua",
-		"javascript",
-		"tsx",
-		"vim",
-		"fish",
-		"c",
-		"c_sharp",
-		"bash",
-		"python",
-		"markdown",
-		"markdown_inline",
-		"astro",
-		"rust",
-		"java",
-	},
+    build = ":TSUpdate",
+    highlight = {
+        enable = true,
+        disable = {},
+    },
+    indent = {
+        enable = true,
+        disable = {},
+    },
+    ensure_installed = {
+        "lua",
+        "javascript",
+        "tsx",
+        "vim",
+        "fish",
+        "c",
+        "c_sharp",
+        "bash",
+        "python",
+        "markdown",
+        "markdown_inline",
+        "astro",
+        "rust",
+        "java",
+        "vimdoc",
+    },
 })
