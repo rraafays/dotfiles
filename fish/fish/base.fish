0efc2ca29d80f13292bb63ccb15e3da3d9155080
raf <rraf@tuta.io>
2024-01-24 16:27:04 +0000

    chore: made visual mode colours match the rest

--- fish/fish/base.fish
+++ fish/fish/base.fish
@@ -45,12 +45,11 @@ if status is-interactive
     set --universal fish_color_error brred
     set --universal fish_color_escape brmagenta
     set --universal fish_color_match red
-    set --universal fish_color_normal FE8019
     set --universal fish_color_operator bryellow --bold
     set --universal fish_color_param brwhite
     set --universal fish_color_quote brgreen
     set --universal fish_color_redirection brmagenta
     set --universal fish_color_search_match yellow
     set --universal fish_color_selection brwhite --bold
-    set --universal fish_color_selection -b 2a1300
+    set --universal fish_color_selection --background=2A1300 FE8019
 end
