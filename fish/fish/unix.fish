2d2426b0921a211417c8c70b7cde56605a2584a6
raf <rraf@tuta.io>
2024-02-28 13:10:10 +0000

    update: wrote command to get iosevka curly

--- fish/fish/unix.fish
+++ fish/fish/unix.fish
@@ -13,23 +13,17 @@ if status --is-interactive
         command doas fish -c "$argv"
     end
 
-    function pacman
-        if type -q paru
-            if count $argv >/dev/null
-                command paru --noconfirm $argv
-            else
-                paru --noconfirm --sync --refresh --sysupgrade
-            end
-        else
-            if count $argv >/dev/null
-                command pacman --noconfirm $argv
-            else
-                if fish_is_root_user
-                    pacman --noconfirm --sync --refresh --sysupgrade
-                else
-                    doas pacman --noconfirm --sync --refresh --sysupgrade
-                end
-            end
-        end
+    function iosevka
+        rm -rf $HOME/.local/share/fonts
+        mkdir $HOME/.local/share/fonts
+        curl -s "https://api.github.com/repos/be5invis/Iosevka/releases/latest" | jq -r ".assets[] | .browser_download_url" | grep -i ttf-iosevkatermcurly- | xargs -n 1 curl -o $HOME/.local/share/fonts/iosevka.zip -L -O --fail --show-error
+        7z -aoa x $HOME/.local/share/fonts/iosevka.zip -o$HOME/.local/share/fonts/
+    end
+
+    function dotfiles
+        rm -rf /home/raf/.config
+        rm -rf /root/.config
+        git clone https://github.com/rraafays/dotfiles /home/raf/.config
+        sudo ln -s /home/raf/.config/ /root/.config
     end
 end
