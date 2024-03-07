70da4a76b09e47f5c5e97a4724ec77d0ddaf6ceb
raf <rraf@tuta.io>
2024-03-07 13:16:45 +0000

    chore: cleanup due to nixos migration

--- fish/fish/unix.fish
+++ fish/fish/unix.fish
@@ -13,19 +13,8 @@ if status --is-interactive
             "$HOME/.config/kitty/unix.conf" >"$HOME/.config/kitty/kitty.conf"
     end
 
-    function doas
-        command doas fish -c "$argv"
-    end
-
     function nsh
         command nix-shell $argv --run fish
-    end
-
-    function iosevka
-        rm -rf $HOME/.local/share/fonts
-        mkdir $HOME/.local/share/fonts
-        curl -s "https://api.github.com/repos/be5invis/Iosevka/releases/latest" | jq -r ".assets[] | .browser_download_url" | grep -i ttf-iosevkatermcurly- | xargs -n 1 curl -o $HOME/.local/share/fonts/iosevka.zip -L -O --fail --show-error
-        7z -aoa x $HOME/.local/share/fonts/iosevka.zip -o$HOME/.local/share/fonts/
     end
 
     function dotfiles
