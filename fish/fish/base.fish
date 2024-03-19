277e6ad83f4541e99667a9926f1dba9047d71591
raf <rraf@tuta.io>
2024-03-19 22:01:36 +0000

    update: testing out detox

--- fish/fish/base.fish
+++ fish/fish/base.fish
@@ -2,6 +2,7 @@ set -x EDITOR nvim
 set -x GH_CONFIG_DIR "$HOME/.config/gh"
 set -x FZF_DEFAULT_COMMAND "rg --files --follow --no-ignore --no-ignore-dot --no-ignore-exclude --no-ignore-files --no-ignore-global --no-ignore-parent --no-ignore-vcs"
 set -x FZF_DEFAULT_OPTS "--color="16,bg+:#000000""
+set -x DETOX_SEQUENCE lower
 
 if status is-interactive
     starship init fish | source
