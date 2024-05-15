481998dd92f7d66895f23f71fa8cdf074e05f080
raf <rraf@tuta.io>
2024-05-15 16:59:33 +0100

    chore: added lspsaga

--- lvim/config.lua
+++ lvim/config.lua
@@ -212,12 +212,52 @@ lvim.keys.insert_mode["<A-`>"] = "`"
 --   },
 -- }
 
+lvim.keys.normal_mode["<Tab>"] = "<cmd>Lspsaga diagnostic_jump_next<cr>"
+lvim.keys.normal_mode["<S-Tab>"] = "<cmd>Lspsaga diagnostic_jump_prev<cr>"
 lvim.plugins = {
     { "chaoren/vim-wordmotion", },
     {
         "kylechui/nvim-surround",
         config = function()
             require("nvim-surround").setup()
+        end
+    },
+    {
+        "glepnir/lspsaga.nvim",
+        config = function()
+            require("lspsaga").setup({
+                server_filetype_map = {},
+                symbol_in_winbar = {
+                    enable = false,
+                },
+                beacon = {
+                    enable = false,
+                },
+                code_action = {
+                    show_server_name = true,
+                },
+                lightbulb = {
+                    enable = false,
+                    enable_in_insert = false,
+                    sign = false,
+                },
+                ui = {
+                    border = "rounded",
+                    colors = {
+                        normal_bg = "#282828",
+                        title_bg = "#EBDBB2",
+                        red = "#fb4833",
+                        green = "#b8ba25",
+                        yellow = "#fabc2e",
+                        blue = "#83a597",
+                        magenta = "#d3859a",
+                        cyan = "#8ec07b",
+                        white = "#ebdbb2",
+                        black = "#282828",
+                    },
+                    actionfix = "󰌵 ",
+                },
+            })
         end
     },
     {
