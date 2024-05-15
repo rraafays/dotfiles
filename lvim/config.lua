91086e8ee7dec8b813f06ce0b304fe8bec8dfcf9
raf <rraf@tuta.io>
2024-05-15 23:16:49 +0100

    chore: added formatters back

--- lvim/config.lua
+++ lvim/config.lua
@@ -247,25 +247,6 @@ lvim.keys.insert_mode["<A-]>"] = "‘"
 lvim.keys.insert_mode["<A-\\>"] = "«"
 lvim.keys.insert_mode["<A-`>"] = "`"
 
--- -- linters and formatters <https://www.lunarvim.org/docs/languages#lintingformatting>
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "stylua" },
---   {
---     command = "prettier",
---     extra_args = { "--print-width", "100" },
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
---   {
---     command = "shellcheck",
---     args = { "--severity", "warning" },
---   },
--- }
-
 lvim.keys.normal_mode["<Tab>"] = "<cmd>Lspsaga diagnostic_jump_next<cr>"
 lvim.keys.normal_mode["<S-Tab>"] = "<cmd>Lspsaga diagnostic_jump_prev<cr>"
 lvim.plugins = {
@@ -427,6 +408,114 @@ lvim.plugins = {
             vim.api.nvim_create_autocmd("FileType", {
                 pattern = "man",
                 command = "Square",
+            })
+        end
+    },
+    {
+        "stevearc/conform.nvim",
+        config = function()
+            local conform = require("conform")
+
+            local function get_os() return vim.loop.os_uname().sysname end
+            local formatter_config_dir = os.getenv("HOME") .. "/.config/nvim/etc/"
+
+            local function get_idea_bin()
+                if get_os() == "Darwin" then return "idea" end
+                if get_os() == "Linux" then return "idea-community" end
+            end
+
+            local slow_format_filetypes = { "java" }
+            local function get_formatters()
+                if os.getenv("FORMAT") ~= "no" then
+                    return {
+                        lua = { "stylua" },
+                        sh = { "shfmt" },
+                        fish = { "fish_indent" },
+                        javascript = { "prettier" },
+                        typescript = { "cognitran" },
+                        typescriptreact = { "cognitran" },
+                        xml = { "xmlformat" },
+                        sql = { "redgate" },
+                        java = { "intellij" },
+                        json = { "prettier" },
+                        html = { "prettier" },
+                        nix = { "nixpkgs_fmt" },
+                    }
+                else
+                    return {}
+                end
+            end
+
+            conform.setup({
+                formatters_by_ft = get_formatters(),
+                formatters = {
+                    intellij = {
+                        command = get_idea_bin(),
+                        require_cwd = false,
+                        stdin = false,
+                        args = {
+                            "format",
+                            "-s",
+                            formatter_config_dir .. "intellij.xml",
+                            "$FILENAME",
+                        },
+                    },
+                    redgate = {
+                        command = "sql-formatter",
+                        stdin = true,
+                        args = {
+                            "--config",
+                            formatter_config_dir .. "redgate.json",
+                        },
+                    },
+                    prettier = {
+                        command = "prettier",
+                        stdin = true,
+                        args = { "--tab-width", 4, "--stdin-filepath", "$FILENAME" },
+                    },
+                    xmlformat = {
+                        command = "xmlformat",
+                        stdin = false,
+                        args = { "--indent", 4, "--overwrite", "$FILENAME" },
+                    },
+                    nixpkgs_fmt = {
+                        command = "nixpkgs-fmt",
+                        stdin = false,
+                        args = { "$FILENAME" },
+                    },
+                    cognitran = {
+                        command = "prettier",
+                        stdin = true,
+                        args = {
+                            "--trailing-comma",
+                            "es5",
+                            "--tab-width",
+                            2,
+                            "--print-width",
+                            100,
+                            "--prose-wrap",
+                            "always",
+                            "--stdin-filepath",
+                            "$FILENAME",
+                        },
+                    },
+                },
+
+                format_on_save = function(bufnr)
+                    if slow_format_filetypes[vim.bo[bufnr].filetype] then return end
+                    local function on_format(err)
+                        if err and err:match("timeout$") then
+                            slow_format_filetypes[vim.bo[bufnr].filetype] = true
+                        end
+                    end
+
+                    return { timeout_ms = 1000, lsp_fallback = false }, on_format
+                end,
+
+                format_after_save = function(bufnr)
+                    if not slow_format_filetypes[vim.bo[bufnr].filetype] then return end
+                    return { lsp_fallback = false }
+                end,
             })
         end
     }
