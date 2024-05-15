1e4cae0d0d53e4cf72cc64edabd09d8224869583
raf <rraf@tuta.io>
2024-05-15 13:22:56 +0100

    chore: added autocenter

--- lvim/config.lua
+++ lvim/config.lua
@@ -111,7 +111,7 @@ lvim.builtin.gitsigns.opts = {
 lvim.plugins = {
     { "chaoren/vim-wordmotion", },
     {
-        "ellisonleao/gruvbox.nvim", -- gruvbox theme
+        "ellisonleao/gruvbox.nvim",
         config = function()
             require("gruvbox").setup({
                 italic = {
@@ -157,4 +157,67 @@ lvim.plugins = {
             })
         end
     },
+    {
+        "shortcuts/no-neck-pain.nvim",
+        config = function()
+            DOCUMENT_WIDTH = 80
+            RECEIPT_WIDTH = 60
+
+            require("no-neck-pain").setup({
+                debug = false,
+                width = DOCUMENT_WIDTH,
+                minSideBufferWidth = 0,
+                disableOnLastBuffer = true,
+                killAllBuffersOnDisable = true,
+                autocmds = {
+                    enableOnVimEnter = true,
+                    enableOnTabEnter = true,
+                    reloadOnColorSchemeChange = true,
+                },
+                mappings = {
+                    enabled = true,
+                    widthUp = "=",
+                    widthDown = "-",
+                    scratchPad = "false",
+                },
+            })
+
+            vim.api.nvim_create_user_command("Center", function()
+                local filetype = vim.bo.filetype
+                if filetype == "xxd" or filetype == "alpha" then
+                    vim.cmd("NoNeckPainResize " .. DOCUMENT_WIDTH)
+                    return
+                elseif filetype == "text" then
+                    vim.cmd("NoNeckPainResize " .. RECEIPT_WIDTH)
+                    return
+                end
+                local path = vim.fn.expand("%:p")
+                local command = io.popen("wc -L " .. path:gsub(" ", "\\ "))
+                if command ~= nil then
+                    local result = command:read("*a")
+                    for i in string.gmatch(result, "%S+") do
+                        if tonumber(i) ~= nil then vim.cmd("NoNeckPainResize " .. i + 2) end
+                    end
+                end
+            end, {})
+
+            vim.api.nvim_create_user_command("Square", function()
+                local command = io.popen("tput lines")
+                if command ~= nil then
+                    local result = command:read("*a")
+                    vim.cmd("NoNeckPainResize " .. result * 2)
+                end
+            end, {})
+
+            vim.api.nvim_create_autocmd("BufWritePost", {
+                pattern = "",
+                command = "Center",
+            })
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "man",
+                command = "Square",
+            })
+        end
+    }
 }
