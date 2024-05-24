e0d0fca80353b34661384edf527caab189a25017
raf <rraf@tuta.io>
2024-05-24 05:20:37 +0100

    chore: made default session named main, added bindings to print current session name and rename it

--- fish/fish/base.fish
+++ fish/fish/base.fish
@@ -35,11 +35,11 @@ if status is-interactive
     end
 
     if [ -x "$(command -v tmux)" ] && [ -z "$TMUX" ] && not test -z "$DISPLAY"
-        exec tmux new-session -A -s (whoami) >/dev/null 2>&1
+        exec tmux new-session -A -s main >/dev/null 2>&1
     end
 
     if not test -z $SSH_TTY
-        exec tmux new-session -A -s (whoami) >/dev/null 2>&1
+        exec tmux new-session -A -s main >/dev/null 2>&1
     end
 
     fish_vi_key_bindings
