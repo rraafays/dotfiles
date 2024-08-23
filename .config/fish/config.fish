01d15472f7f29c7abe883212a924bbb8541be298
raf <rraf@tuta.io>
2024-08-23 12:30:45 +0100

    chore: using the src structure instead of nested fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,15 @@
 set FISH_HOME "$HOME/.config/fish"
 
-source $FISH_HOME/fish/base.fish
-source $FISH_HOME/fish/aliases.fish
-source $FISH_HOME/fish/functions.fish
-source $FISH_HOME/fish/path.fish
-source $FISH_HOME/fish/credentials.fish
-source $FISH_HOME/fish/ls.fish
+source $FISH_HOME/src/base.fish
+source $FISH_HOME/src/aliases.fish
+source $FISH_HOME/src/functions.fish
+source $FISH_HOME/src/path.fish
+source $FISH_HOME/src/credentials.fish
+source $FISH_HOME/src/ls.fish
 
 switch (uname)
     case Darwin
-        source $FISH_HOME/fish/macos.fish
+        source $FISH_HOME/src/macos.fish
     case Linux
-        source $FISH_HOME/fish/unix.fish
+        source $FISH_HOME/src/unix.fish
 end
