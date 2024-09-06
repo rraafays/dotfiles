c545e3a8d870ffcc94fa06e8d159baba75311196
raf <rraf@tuta.io>
2024-09-06 18:27:01 +0100

    update: wrote function to list git modules

--- .config/fish/src/functions.fish
+++ .config/fish/src/functions.fish
@@ -31,6 +31,14 @@ function src
     end
 end
 
+function modules
+    if test -e .gitmodules
+        grep "path|url" .gitmodules | awk '{print $3}' | sed 'N;s/\n/ -> /' |bat -p
+    else
+        echo "there are no modules!"
+    end
+end
+
 function nsh
     nix-shell $argv
 end
