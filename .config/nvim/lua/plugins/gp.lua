3b6c02ad8e37d13d9e1d450e4b333ab134f93886
raf <rraf@tuta.io>
2024-09-10 14:17:33 +0100

    fix: had a double bind on tab

--- .config/nvim/lua/plugins/gp.lua
+++ .config/nvim/lua/plugins/gp.lua
@@ -11,7 +11,7 @@ return {
             }
         end
         vim.keymap.set({ "n", "i", "v" }, "<Space>", "<cmd>GpRewrite<cr>", keymapOptions("Inline rewrite"))
-        vim.keymap.set({ "n", "i", "v" }, "<Tab>", "<cmd>GpImplement<cr>", keymapOptions("Implement selection"))
+        vim.keymap.set({ "n", "i", "v" }, "`", "<cmd>GpImplement<cr>", keymapOptions("Implement selection"))
         vim.keymap.set({ "n", "i", "v" }, "<C-n>", "<cmd>GpEnew<cr>", keymapOptions("Implement selection"))
         vim.keymap.set({ "n", "i", "v", "x" }, "<C-k>", "<cmd>GpStop<cr>", keymapOptions("Stop"))
         require("gp").setup(conf)
