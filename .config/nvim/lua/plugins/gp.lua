08f64046fb1fec9e148a7110620b7605f4bcf641
raf <rraf@tuta.io>
2024-09-11 10:06:40 +0100

    chore: correctly provided selection

--- .config/nvim/lua/plugins/gp.lua
+++ .config/nvim/lua/plugins/gp.lua
@@ -10,9 +10,10 @@ return {
                 desc = "GPT prompt " .. desc,
             }
         end
-        vim.keymap.set({ "n", "i", "v" }, "<Space>", "<cmd>GpRewrite<cr>", keymapOptions("Inline rewrite"))
-        vim.keymap.set({ "n", "i", "v" }, "`", "<cmd>GpImplement<cr>", keymapOptions("Implement selection"))
-        vim.keymap.set({ "n", "i", "v" }, "<C-n>", "<cmd>GpEnew<cr>", keymapOptions("Implement selection"))
+        vim.keymap.set({ "n", "i" }, "<Space>", "<cmd>GpRewrite<cr>", keymapOptions("Inline rewrite"))
+        vim.keymap.set({ "n", "i" }, "<C-n>", "<cmd>GpEnew<cr>", keymapOptions("Implement selection"))
+        vim.keymap.set("v", "<Space>", ":'<,'>GpRewrite<cr>", keymapOptions("Inline rewrite"))
+        vim.keymap.set("v", "<C-n>", ":'<,'>GpEnew<cr>", keymapOptions("Implement selection"))
         vim.keymap.set({ "n", "i", "v", "x" }, "<C-k>", "<cmd>GpStop<cr>", keymapOptions("Stop"))
         require("gp").setup(conf)
     end,
