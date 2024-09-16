04422efffa02de4ae2134e57d1c9f0a40828c26d
raf <rraf@tuta.io>
2024-09-16 11:47:37 +0100

    chore: added missing cmp-path plugin

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,5 +1,8 @@
 return {
     "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-path",
+    },
     config = function()
         local cmp = require("cmp")
         local lspkind = require("lspkind")
