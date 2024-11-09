b7a0cb9e0ebec886c5cae9472abafd899484a9bf
raf <rraf@tuta.io>
2024-11-09 11:39:54 +0000

    Revert "update: switched to material you and pywal"
    This reverts commit 74af1968a7291149120bca590b2b3b70451a0cf0.

--- .config/nvim/lua/plugins/pywal.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "uZer/pywal16.nvim",
-    lazy = false,
-    config = function()
-        local pywal16 = require("pywal16")
-        pywal16.setup()
-        vim.cmd([[colorscheme pywal16]])
-    end,
-}
