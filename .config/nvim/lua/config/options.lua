03b3e757c36cbeafb02688afb29e28db0b555205
raf <rraf@tuta.io>
2025-01-13 12:42:23 +0000

    chore: agnostic nvim theme

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,5 +1,5 @@
 vim.opt.statusline = "%{repeat('─',winwidth('.'))}"
-vim.opt.laststatus = 3
+vim.opt.termguicolors = false
 vim.wo.number = false
 vim.opt.virtualedit = "block"
 vim.opt.shiftwidth = 4
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,8 +3,6 @@ return {
     config = function()
         local lualine = require("lualine")
 
-        local theme = require("lualine.themes.gruvbox")
-
         local function search_term()
             local res = vim.fn.searchcount()
             if res.total > 0 then
@@ -14,45 +12,9 @@ return {
             end
         end
 
-        theme.normal.a.fg = "#A89983"
-        theme.insert.a.fg = "#448488"
-        theme.replace.a.fg = "#FB4833"
-        theme.visual.a.fg = "#FE8019"
-        theme.command.a.fg = "#B8BA25"
-        theme.normal.a.bg = "none"
-        theme.insert.a.bg = "none"
-        theme.replace.a.bg = "none"
-        theme.visual.a.bg = "none"
-        theme.command.a.bg = "none"
-
-        theme.inactive.b.fg = "#EBDBB2"
-        theme.normal.b.fg = "#EBDBB2"
-        theme.insert.b.fg = "#EBDBB2"
-        theme.replace.b.fg = "#EBDBB2"
-        theme.visual.b.fg = "#EBDBB2"
-        theme.command.b.fg = "#EBDBB2"
-        theme.inactive.b.bg = "none"
-        theme.normal.b.bg = "none"
-        theme.insert.b.bg = "none"
-        theme.replace.b.bg = "none"
-        theme.visual.b.bg = "none"
-        theme.command.b.bg = "none"
-
-        theme.inactive.c.fg = "#A89983"
-        theme.normal.c.fg = "#A89983"
-        theme.insert.c.fg = "#A89983"
-        theme.replace.c.fg = "#A89983"
-        theme.visual.c.fg = "#A89983"
-        theme.command.c.fg = "#A89983"
-        theme.inactive.c.bg = "none"
-        theme.normal.c.bg = "none"
-        theme.insert.c.bg = "none"
-        theme.replace.c.bg = "none"
-        theme.visual.c.bg = "none"
-        theme.command.c.bg = "none"
-
         lualine.setup({
             options = {
+                globalstatus = true,
                 icons_enabled = true,
                 component_separators = {
                     left = "",
@@ -63,7 +25,6 @@ return {
                     right = "",
                 },
                 disabled_filetypes = {},
-                theme = theme,
             },
             sections = {
                 lualine_a = {
