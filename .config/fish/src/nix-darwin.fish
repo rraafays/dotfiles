997d4b0631adf373a296856f0766bf15f3c4e897
raf <rraf@tuta.io>
2025-02-13 22:14:23 +0000

    chore: before-nix-darwin

--- .config/fish/src/nix-darwin.fish
+++ .config/fish/src/nix-darwin.fish
@@ -8,6 +8,17 @@ function rebuild
         nix-shell --packages stow --run "stow ."
         cd - || exit
     else
+        function before-nix-darwin
+            for file in /etc/nix/nix*
+                set new_name "$file.before-nix-darwin"
+                mv "$file" "$new_name"
+            end
+        end
+        if not fish_is_root_user
+            sudo before-nix-darwin
+        else
+            before-nix-darwin
+        end
         darwin-rebuild $argv -I "darwin-config=/etc/nix-darwin/configuration.nix"
         if test -e result
             rm result
