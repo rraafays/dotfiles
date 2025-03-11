916f1f6dfe59ca021bdc7c166bedec4de874e714
raf <rraf@tuta.io>
2025-03-11 11:02:26 +0000

    chore: rebuild upgrade

--- .config/fish/src/nix-darwin.fish
+++ .config/fish/src/nix-darwin.fish
@@ -1,16 +1,23 @@
 nix-your-shell fish | source
 
 function rebuild
-    if [ "$argv[1]" = config ]
-        cd ~/.config/ || exit
-        rm -rf "$(ls -1t .config/)"
-        cd .config/ || exit
-        nix-shell --packages stow --run "stow ."
-        cd - || exit
-    else
-        darwin-rebuild $argv -I "darwin-config=/etc/nix-darwin/configuration.nix"
-        if test -e result
-            rm result
-        end
+    switch "$argv[1]"
+        case config
+            cd ~/.config/ || exit
+            rm -rf "$(ls -1t .config/)"
+            cd .config/ || exit
+            nix-shell --packages stow --run "stow ."
+            cd - || exit
+        case upgrade
+            sudo nix-channel --update
+            darwin-rebuild switch $argv[2..-1] -I "darwin-config=/etc/nix-darwin/configuration.nix"
+            if test -e result
+                rm result
+            end
+        case '*'
+            darwin-rebuild $argv -I "darwin-config=/etc/nix-darwin/configuration.nix"
+            if test -e result
+                rm result
+            end
     end
 end
