10812f01527b07608ee271f50a28bd21f524ecd8
raf <rraf@tuta.io>
2025-03-25 00:03:59 +0000

    chore: remove fetch

--- .config/fish/src/functions.fish
+++ .config/fish/src/functions.fish
@@ -24,14 +24,6 @@ function clean
     end
 end
 
-function fetch
-    if test -e .git
-        nix-shell --packages onefetch --run "onefetch --true-color never $argv"
-    else
-        nix-shell --packages neofetch --run "neofetch $argv"
-    end
-end
-
 function qr
     nix-shell --packages qrencode --run "qrencode -t UTF8 -m 2 '$argv'"
 end
