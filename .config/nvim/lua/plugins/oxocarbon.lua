d65cc70d1d9a9349520498452d2be7432c286d04
raf <rraf@tuta.io>
2025-04-29 14:00:55 +0100

    chore: updated plugins and changed gutter colour

--- .config/nvim/lua/plugins/oxocarbon.lua
+++ .config/nvim/lua/plugins/oxocarbon.lua
@@ -7,5 +7,7 @@ return {
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
     vim.api.nvim_set_hl(0, "Visual", { bg = "#FFE97B", fg = "#000000" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" }) -- Change gutter color
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "#808080", bg = "#000000" }) -- Change line numbers color
   end,
 }
