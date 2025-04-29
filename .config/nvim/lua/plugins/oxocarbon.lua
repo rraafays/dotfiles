58095070e6cce3e88734bba0d587219c9445cc90
raf <rraf@tuta.io>
2025-04-29 23:51:16 +0100

    chore: darker brblack

--- .config/nvim/lua/plugins/oxocarbon.lua
+++ .config/nvim/lua/plugins/oxocarbon.lua
@@ -8,6 +8,6 @@ return {
     vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
     vim.api.nvim_set_hl(0, "Visual", { bg = "#FFE97B", fg = "#000000" })
     vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" }) -- Change gutter color
-    vim.api.nvim_set_hl(0, "LineNr", { fg = "#808080", bg = "#000000" }) -- Change line numbers color
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "#525252", bg = "#000000" }) -- Change line numbers color
   end,
 }
