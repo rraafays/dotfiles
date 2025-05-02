82670a5e2563d073a92c5244cff7b283b75d9bde
raf <rraf@tuta.io>
2025-05-02 21:54:02 +0100

    chore: added fzf

---
+++ .config/fish/functions/fzf.fish
@@ -0,0 +1,3 @@
+function fzf
+    command fzf --color=fg:#848d97,bg:#000000,hl:#ffffff --color=fg+:#e6edf3,bg+:#000000,hl+:#ffa657 --color=info:#d29922,prompt:#2f81f7,pointer:#a371f7 --color=marker:#3fb950,spinner:#6e7681,header:#343941
+end
