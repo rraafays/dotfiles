f5a90dc17867196196ac0b47765621b10b30fca6
raf <rraf@tuta.io>
2025-05-04 11:16:11 +0100

    chore: transparent bg

--- .config/nvim/lua/plugins/github-dark.lua
+++ .config/nvim/lua/plugins/github-dark.lua
@@ -10,6 +10,14 @@ return {
       },
     })
     vim.cmd("colorscheme github_dark_default")
+    vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "MsgArea", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "NONE" })
+    vim.opt.fillchars = { eob = " " }
+
     vim.api.nvim_set_hl(0, "Visual", { bg = "#FFFFFF", fg = "#000000" })
     vim.api.nvim_set_hl(0, "CursorLine", { bg = "#000000" })
     vim.api.nvim_set_hl(0, "Cursor", { bg = "#FFFFFF", fg = "#000000" })
