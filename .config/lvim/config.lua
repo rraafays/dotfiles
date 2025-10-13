984f236780d6f6d361880d15171773dc9bfd8197
raf <rraf@tuta.io>
2025-10-13 12:27:30 +0100

    chore: auto format

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -31,41 +31,43 @@ lvim.plugins = {
       vim.api.nvim_set_hl(0, "CursorLine", { bg = "#000000" })
       vim.api.nvim_set_hl(0, "Cursor", { bg = "#FFFFFF", fg = "#000000" })
     end,
-    },
+  },
 }
 
-    local theme = {
-      inactive = {
-        a = { fg = "#33B1FF", bg = "#000000", gui = "bold" },
-        b = { fg = "#bc8cff", bg = "#000000", gui = "bold" },
-        c = { fg = "#FFFFFF", bg = "#000000" },
-        z = { fg = "#484f58", bg = "#000000" },
-      },
-      normal = {
-        a = { fg = "#484f58", bg = "#000000", gui = "bold" },
-        b = { fg = "#bc8cff", bg = "#000000", gui = "bold" },
-        c = { fg = "#FFFFFF", bg = "#000000" },
-        z = { fg = "#484f58", bg = "#000000" },
-      },
-      visual = {
-        a = { fg = "#d29922", bg = "#000000", gui = "bold" },
-        b = { fg = "#bc8cff", bg = "#000000", gui = "bold" },
-        c = { fg = "#FFFFFF", bg = "#000000" },
-        z = { fg = "#484f58", bg = "#000000" },
-      },
-      insert = {
-        a = { fg = "#58a6ff", bg = "#000000", gui = "bold" },
-        b = { fg = "#bc8cff", bg = "#000000", gui = "bold" },
-        c = { fg = "#FFFFFF", bg = "#000000" },
-        z = { fg = "#484f58", bg = "#000000" },
-      },
-      replace = {
-        a = { fg = "#ff7b72", bg = "#000000", gui = "bold" },
-        b = { fg = "#bc8cff", bg = "#000000", gui = "bold" },
-        c = { fg = "#FFFFFF", bg = "#000000", gui = "bold" },
-        z = { fg = "#484f58", bg = "#000000", gui = "bold" },
-      },
-      command = { a = { fg = "#BE95FF", bg = "#000000", gui = "bold" } },
-    }
+local theme = {
+  inactive = {
+    a = { fg = "#33B1FF", bg = "#000000", gui = "bold" },
+    b = { fg = "#bc8cff", bg = "#000000", gui = "bold" },
+    c = { fg = "#FFFFFF", bg = "#000000" },
+    z = { fg = "#484f58", bg = "#000000" },
+  },
+  normal = {
+    a = { fg = "#484f58", bg = "#000000", gui = "bold" },
+    b = { fg = "#bc8cff", bg = "#000000", gui = "bold" },
+    c = { fg = "#FFFFFF", bg = "#000000" },
+    z = { fg = "#484f58", bg = "#000000" },
+  },
+  visual = {
+    a = { fg = "#d29922", bg = "#000000", gui = "bold" },
+    b = { fg = "#bc8cff", bg = "#000000", gui = "bold" },
+    c = { fg = "#FFFFFF", bg = "#000000" },
+    z = { fg = "#484f58", bg = "#000000" },
+  },
+  insert = {
+    a = { fg = "#58a6ff", bg = "#000000", gui = "bold" },
+    b = { fg = "#bc8cff", bg = "#000000", gui = "bold" },
+    c = { fg = "#FFFFFF", bg = "#000000" },
+    z = { fg = "#484f58", bg = "#000000" },
+  },
+  replace = {
+    a = { fg = "#ff7b72", bg = "#000000", gui = "bold" },
+    b = { fg = "#bc8cff", bg = "#000000", gui = "bold" },
+    c = { fg = "#FFFFFF", bg = "#000000", gui = "bold" },
+    z = { fg = "#484f58", bg = "#000000", gui = "bold" },
+  },
+  command = { a = { fg = "#BE95FF", bg = "#000000", gui = "bold" } },
+}
+
 lvim.builtin.lualine.options.theme = theme
 lvim.builtin.illuminate.active = false
+lvim.format_on_save.enabled = true
