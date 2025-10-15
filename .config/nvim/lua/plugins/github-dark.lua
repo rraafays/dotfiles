a13b6fbb7794fa0e702f16d70af3528d6ee16a56
raf <rraf@tuta.io>
2025-10-15 18:21:14 +0100

    update: unified theme

---
+++ .config/nvim/lua/plugins/github-dark.lua
@@ -0,0 +1,53 @@
+return {
+  { "snacks.nvim", opts = { indent = { enabled = false } } },
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    priority = 9999,
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+          terminal_colors = false,
+          dim_inactive = false,
+          module_default = true,
+          styles = {
+            comments = "NONE",
+            functions = "NONE",
+            keywords = "NONE",
+            variables = "NONE",
+            conditionals = "NONE",
+            constants = "NONE",
+            numbers = "NONE",
+            operators = "NONE",
+            strings = "NONE",
+            types = "NONE",
+          },
+        },
+      })
+
+      -- normal
+      vim.g.terminal_color_0 = "#0d1116"
+      vim.g.terminal_color_1 = "#ff7b72"
+      vim.g.terminal_color_2 = "#3fba50"
+      vim.g.terminal_color_3 = "#d29922"
+      vim.g.terminal_color_4 = "#58a6ff"
+      vim.g.terminal_color_5 = "#bc8cff"
+      vim.g.terminal_color_6 = "#77e3ea"
+      vim.g.terminal_color_7 = "#b1bac4"
+
+      -- bright
+      vim.g.terminal_color_8 = "#161b22"
+      vim.g.terminal_color_9 = "#ffa198"
+      vim.g.terminal_color_10 = "#56d364"
+      vim.g.terminal_color_11 = "#e4b340"
+      vim.g.terminal_color_12 = "#79c0ff"
+      vim.g.terminal_color_13 = "#d2a8ff"
+      vim.g.terminal_color_14 = "#b3f0ff"
+      vim.g.terminal_color_15 = "#e6edf3"
+
+      vim.cmd("colorscheme github_dark")
+    end,
+  },
+}
