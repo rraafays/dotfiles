bdf503c01eeb291308961a439b28d8de88b900c6
raf <rraf@tuta.io>
2025-10-22 19:57:28 +0100

    chore: inlay hint

--- .config/nvim/lua/plugins/language-servers.lua
+++ .config/nvim/lua/plugins/language-servers.lua
@@ -33,6 +33,14 @@ return {
         float = {
           border = "rounded",
         },
+        underline = true,
+        update_in_insert = false,
+        virtual_text = {
+          spacing = 4,
+          source = "if_many",
+          prefix = "●",
+        },
+        severity_sort = true,
         signs = {
           text = {
             [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
