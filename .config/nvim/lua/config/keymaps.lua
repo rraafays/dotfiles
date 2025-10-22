a59fd343759b15cc7e1ccaee364efebbb94869e6
raf <rraf@tuta.io>
2025-10-22 20:33:00 +0100

    chore: disable tabline, expand keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,16 @@
 local map = vim.keymap.set
 
-map("n", "<f2>", "<cmd>lua vim.diagnostic.goto_next()<cr>")
-map("n", "<S-f2>", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+map("n", "<F2>", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+map("n", "<S-F2>", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+
+map("n", "<M-1>", Snacks.picker.explorer)
+
+map("n", "<M-F12>", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end)
+map("t", "<M-F12>", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end)
+map("n", "<C-f>", function()
+  Snacks.picker.files()
+end)
--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "mason-org/mason.nvim", enabled = false },
   { "mason-org/mason-lspconfig.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
 }
